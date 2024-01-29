f0c4516c579e5efc4b2828c82d691d0ef357c097
HazelArsya03 <hazelarsya03@gmail.com>
2024-01-29 14:05:03 +0700

    pengkondisian js

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("Masukkan Angka: ");
+// alert("selamat datang" + nama);
+
+if (x % 2 == 0) {
+    alert(x + " bilangan genap");
+} else if(x % 2 == 1){
+    alert(x + " bilangan ganjil")
+} else {
+    alert("ITU BUKAN ANGKA!!");
+}
